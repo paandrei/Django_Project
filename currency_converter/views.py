0d0cf65e72a6a461047c11e644fbfd387f829164
Puschi <puscasu.a.a@gmail.com>
2023-11-16 17:34:21 +0200

    third commit

--- currency_converter/views.py
+++ currency_converter/views.py
@@ -23,15 +23,18 @@ def save_currency_exchang():
 
 def show(request):
     if request.method == 'POST':
-        amount = float(request.POST.get('amount'))
-        currency_from = request.POST.get('currency_from')
-        currency_to = request.POST.get('currency_to')
-        exchange_rates = requests.get("https://openexchangerates.org/api/latest.json?app_id=75a656f72868461692765f2865ec8e85")
-        rates = exchange_rates.json()['rates']
-        dollar_to_currencyt_from = float(rates[currency_from])
-        dollar_to_currencyt_to = float(rates[currency_to])
-        rates = amount*(dollar_to_currencyt_to/dollar_to_currencyt_from)
-        return render(request,"currency_converter/exchange.html", {'currency_data': transform_data(), 'result':('%.2f')%rates, 'currency_to':currency_to})
+        try:
+            amount = float(request.POST.get('amount'))
+            currency_from = request.POST.get('currency_from')
+            currency_to = request.POST.get('currency_to')
+            exchange_rates = requests.get("https://openexchangerates.org/api/latest.json?app_id=75a656f72868461692765f2865ec8e85")
+            rates = exchange_rates.json()['rates']
+            dollar_to_currencyt_from = float(rates[currency_from])
+            dollar_to_currencyt_to = float(rates[currency_to])
+            rates = amount*(dollar_to_currencyt_to/dollar_to_currencyt_from)
+            return render(request,"currency_converter/exchange.html", {'currency_data': transform_data(), 'result':('%.2f')%rates, 'currency_to':currency_to})
+        except BaseException:
+            return render(request,"currency_converter/exchange.html", {'currency_data': transform_data()})
     else:
          return render(request,"currency_converter/exchange.html", {'currency_data': transform_data()})
 
